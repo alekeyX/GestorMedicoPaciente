6ee162d9edf4b113438e4150e3f9fb01ec66fef6
Alejandro Rojas <alejandrod.rojasg@gmail.com>
2020-08-04 11:42:14 -0400

    registro de la zona horaria en el schema

--- src/models/Message.js
+++ src/models/Message.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 
+const date = Date()
+
 const MessageSchema = mongoose.Schema ({
     patient_id:     { type: mongoose.Schema.Types.ObjectId, ref: 'Patient' },
     medic_id:       { type: mongoose.Schema.Types.ObjectId, ref: 'Medic' },
     msg:            { type: String },
     from:           { type: String },
-}, {
-    timestamps: true
+    createdAt:      { type: String, default: date}
 })
 
 const Message = mongoose.model('Message', MessageSchema)
