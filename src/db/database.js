1cc5a2afbae73b85c1c0d8e94c58d8e5ed26ef9f
Alejandro Rojas <alejandrod.rojasg@gmail.com>
2020-12-23 15:44:51 -0400

    mongoAtlas ha sido agregado

--- src/db/database.js
+++ src/db/database.js
@@ -1,5 +1,6 @@
 module.exports = {
     port: process.env.PORT || 4000,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/consultorio',
+    // db: process.env.MONGODB || 'mongodb://localhost:27017/consultorio',
+    db: process.env.MONGODB || 'mongodb+srv://alekey:<alekey>@medicopaciente.ddape.mongodb.net/<MedicoPaciente>?retryWrites=true&w=majority',
     SECRET_TOKEN: 'claveDeAccesoTokenParaConsultorioWeb'
 }
