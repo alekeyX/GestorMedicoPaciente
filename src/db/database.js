9f0c806b2a7f7ad27b8d0fcc6a27596ac605674d
Alejandro Rojas <alejandrod.rojasg@gmail.com>
2021-09-03 21:14:34 -0400

    cambio de ireccion de base de datos

--- src/db/database.js
+++ src/db/database.js
@@ -1,8 +1,8 @@
 module.exports = {
     port: process.env.PORT || 4000,
     // direccion de base de datos local
-    db: process.env.MONGODB || 'mongodb://localhost:27017/centro_db',
+    // db: process.env.MONGODB || 'mongodb://localhost:27017/centro_db',
     // direccion de base de datos en la nube
-    // db: process.env.MONGODB || 'mongodb+srv://alekey:alekey@medicopaciente.ddape.mongodb.net/consultorio?retryWrites=true&w=majority',
+    db: process.env.MONGODB || 'mongodb+srv://alekey:alekey@medicopaciente.ddape.mongodb.net/consultorio?retryWrites=true&w=majority',
     SECRET_TOKEN: process.env.SECRET_TOKEN || 'claveDeAccesoTokenParaConsultorioWeb'
 }
