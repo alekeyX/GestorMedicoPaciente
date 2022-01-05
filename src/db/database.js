1e84da2520fad2511d3f3a9c7b0f3f8dbd697e7d
Alejandro Rojas <alejandrod.rojasg@gmail.com>
2022-01-05 12:53:34 -0400

    fix

--- src/db/database.js
+++ src/db/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     port: process.env.PORT || 4000,
     // direccion de base de datos local
-    db: process.env.MONGODB || 'mongodb://localhost:27017/consultorioWeb',
+    //db: process.env.MONGODB || 'mongodb://localhost:27017/consultorioWeb',
     // direccion de base de datos en la nube
     db: process.env.MONGODB || 'mongodb+srv://alekey:alekey@medicopaciente.ddape.mongodb.net/consultorio?retryWrites=true&w=majority',
     SECRET_TOKEN: process.env.SECRET_TOKEN || 'claveDeAccesoTokenParaConsultorioWeb'
